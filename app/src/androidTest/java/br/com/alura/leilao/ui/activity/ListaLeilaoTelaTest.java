d1277b11967425fc419a95d63cd1c40f4a0318bd
Rafaela Hristidis Morais <rafaelamorais@brq.com>
2021-04-13 11:46:42 -0300

    leiloes finalizado

---
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -0,0 +1,139 @@
+package br.com.alura.leilao.ui.activity;
+
+import android.content.Intent;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import br.com.alura.leilao.R;
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.api.retrofit.client.TesteWebClient;
+import br.com.alura.leilao.formatter.FormatadorDeMoeda;
+import br.com.alura.leilao.model.Leilao;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+public class ListaLeilaoTelaTest {
+
+    private static final String ERRO_FALHA_LIMPEZA_DE_DADOS_DA_API = "Banco de dados não foi limpo";
+    private static final String LEILAO_NAO_FOI_SALVO = "Leilão não foi salvo: ";
+    @Rule
+    public ActivityTestRule activity =
+            new ActivityTestRule<>(ListaLeilaoActivity.class, true, true);
+    private final TesteWebClient webClient = new TesteWebClient();
+    private final FormatadorDeMoeda formatadorDeMoeda = new FormatadorDeMoeda();
+
+    @Before
+    public void setup() throws IOException {
+        limpaBancoDeDadosDaApi();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        limpaBancoDeDadosDaApi();
+    }
+
+    @Test
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(new Leilao("Carro"));
+
+        activity.launchActivity(new Intent());
+
+        //     Thread.sleep(1000);
+
+        onView(allOf(withText("Carro"), withId(R.id.item_leilao_maior_lance)))
+                .check(matches(isDisplayed()));
+
+        String formatoEsperado = formatadorDeMoeda.formata(0.0);
+
+        onView(allOf(withText(formatoEsperado), withId(R.id.item_leilao_maior_lance))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void deve_AparecerDoisLeiloes_QuandoCarregarDoisLeiloesDaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(
+                new Leilao("Carro"),
+                new Leilao("Computador"));
+
+        activity.launchActivity(new Intent());
+
+        /*
+        onView(allOf(withText("Carro"), withId(R.id.item_leilao_descricao)))
+                .check(matches(isDisplayed()));
+
+        String formatoEsperadoParaCarro = formatadorDeMoeda.formata(0.00);
+        onView(allOf(withText(formatoEsperadoParaCarro), withId(R.id.item_leilao_maior_lance))).check(matches(isDisplayed()));
+
+        onView(allOf(withText("Computador"), withId(R.id.item_leilao_descricao)))
+                .check(matches(isDisplayed()));
+
+        String formatoEsperadoParaComputador = formatadorDeMoeda.formata(0.00);
+        onView(allOf(withText(formatoEsperadoParaComputador), withId(R.id.item_leilao_maior_lance))).check(matches(isDisplayed()));
+    */
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .check(matches(apareceLeilao(0, "Carro", 0.00)));
+
+
+
+    }
+
+    private Matcher<? super View> apareceLeilao(final int posicao, final String descricaoEsperada,
+                                                final double maiorLanceEsperado
+                                                ) {
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+            @Override
+            protected boolean matchesSafely(RecyclerView item) {
+                View viewDoViewHolder = item.findViewHolderForAdapterPosition(posicao).itemView;
+                View textViewDescricao = viewDoViewHolder.findViewById(R.id.item_leilao_descricao);
+                boolean temDescricaoEsperada = textViewDescricao.toString().equals(descricaoEsperada);
+
+                TextView textViewMaiorLance = viewDoViewHolder.findViewById(R.id.item_leilao_maior_lance);
+                FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+                boolean temMaiorLanceEsperado = textViewMaiorLance.getText().toString()
+                        .equals(formatador.formata(maiorLanceEsperado));
+
+                return temDescricaoEsperada && temMaiorLanceEsperado;
+            }
+        };
+    }
+
+
+    private void limpaBancoDeDadosDaApi() throws IOException {
+        boolean bancoDeDadosNaoFoiLimpo = !webClient.limpaBancoDeDados();
+        if (bancoDeDadosNaoFoiLimpo) {
+            Assert.fail(ERRO_FALHA_LIMPEZA_DE_DADOS_DA_API);
+        }
+    }
+
+    private void tentaSalvarLeilaoNaApi(Leilao... leiloes) throws IOException {
+        for (Leilao leilao : leiloes) {
+            Leilao leilaoSalvo = webClient.salva(leilao);
+            if (leilaoSalvo == null) {
+                Assert.fail(LEILAO_NAO_FOI_SALVO + leilao.getDescricao());
+            }
+        }
+    }
+}
